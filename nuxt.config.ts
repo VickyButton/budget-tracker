84ecd0c6baac660e2f811b3fac48b2e8d8dee4a9
Vicky <vickythewebdev@gmail.com>
2025-08-23 03:41:57 -0400

    [Setup] Install vitest and uuid

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
-  imports: {
-    scan: false,
-  },
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
   devtools: {
     enabled: true,
   },
