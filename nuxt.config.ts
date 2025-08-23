1db9aaa7edbeb0c83704c5bee3d00ee4aae0c7b2
Vicky <vickythewebdev@gmail.com>
2025-08-23 04:11:51 -0400

    [Chore] Install tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module', '@nuxtjs/tailwindcss'],
   devtools: {
     enabled: true,
   },
