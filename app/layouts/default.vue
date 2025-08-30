fcabac6f1815eee57705b8c5c43beacd2854f8c8
Vicky <vickythewebdev@gmail.com>
2025-08-30 00:42:34 -0400

    [Feature] Add default layout and update App component

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    id="layout"
+    class="flex flex-col"
+  >
+    <header class="p-4">
+      <TextHeading>
+        <slot name="header" />
+      </TextHeading>
+    </header>
+    <main>
+      <slot name="content" />
+    </main>
+  </div>
+</template>
